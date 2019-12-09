9ea9b1db3082958b6897a43fde13453ed4ed3607
jose gonzalez <jose@joses-MBP.grandenetworks.net>
2019-12-09 11:44:17 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,16 +26,32 @@ var cards = [
 
 var cardsInPlay = [];
 
-function flipCard() {
-	console.log("User flipped") + cards[cardId].rank;
-	cardsInPlay.push(cards[cardId].rank);
-}
-console.log(cards[cardId].suit)
-/*
+
+function checkformatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 alert("You Found A Match");
 }
 else {
 alert("Sorry, Try Again");
-}
-*/
+};
+};
+
+function flipCard() {
+	var cardId = this.getattribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setattribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){
+		checkformatch();
+	};
+};
+
+
+function createboard(){
+	for (var i = 0; i < cards.length; i++){
+		var cardelement = document.createelement('img');
+		cardelement.setattribute('src', 'images/back.png');
+		cardelement.setattribute('data-id', i);
+		cardelement.addeventlistener('click', flipcard);
+		cardtable.appendchild(cardelement);
+	}
+	};
